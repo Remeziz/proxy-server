341cb0df8305f8cd508eb4581ddbe2aab7452963
Mykola Zelinskiy <79668248+Remeziz@users.noreply.github.com>
2025-01-30 12:35:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,8 +21,11 @@ access_log /var/log/squid/access.log squid
 cache_log /var/log/squid/cache.log
 EOF
 
+# Append to /etc/security/limits.conf
+echo "* - nofile 4096" | sudo tee -a /etc/security/limits.conf
+
 # Enable and start the Squid service
-sudo systemctl enable squid
-sudo systemctl reload squid
+sudo systemctl enable squid.service
+sudo systemctl restart squid.service
 
 echo "HTTP proxy installation and configuration complete without authentication."
